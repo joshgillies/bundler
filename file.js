04f4bccf2298d89f75545680e7bca0e329aaf64f
Josh Gillies <github@joshgilli.es>
2015-07-25 23:48:03 +1000

    Add support for setting file asset names

--- file.js
+++ file.js
@@ -1,5 +1,7 @@
 var WeakmapEvent = require('weakmap-event')
+var document = require('global/document')
 var prettyBytes = require('pretty-bytes')
+var nextTick = require('next-tick')
 var cuid = require('cuid')
 var hg = require('mercury')
 var h = require('mercury').h
@@ -12,26 +14,67 @@ function File (opts) {
   return hg.state({
     id: hg.value(opts.id || cuid()),
     path: hg.value(opts.path || ''),
-    title: hg.value(opts.title || opts.path || ''),
+    editing: hg.value(opts.editing || false),
+    title: hg.value(opts.title || ''),
     size: hg.value(opts.size || 0),
-    contents: hg.value(opts.contents || '')
+    contents: hg.value(opts.contents || ''),
+    channels: {
+      startEdit: function startEdit (state) {
+        console.log(state.editing())
+        state.editing.set(true)
+      },
+      finishEdit: function finishEdit (state, data) {
+        if (state.editing() === false) {
+          return
+        }
+
+        state.editing.set(false)
+        state.title.set(data.title)
+      }
+    }
   })
 }
 
 File.onRemove = RemoveEvent.listen
 
-File.render = function render (state, parentHandles) {
-  return h('li', [
-    h('span', state.path + ' (' + prettyBytes(state.size) + ')'),
+File.render = function render (file, parentHandles) {
+  return h('li', { key: file.id }, [
+    (file.enditing ? h('input', {
+      type: 'text',
+      value: file.title || 'test',
+      name: 'title',
+      'ev-focus': file.editing ? FocusHook() : null,
+      'ev-event': hg.sendSubmit(file.channels.finishEdit),
+      'ev-blur': hg.sendValue(file.channels.finishEdit)
+    }) : (file.title ? h('span', {
+      'ev-dblclick': hg.send(file.channels.startEdit)
+    }, file.title) : null)),
+    h('span', file.path + ' (' + prettyBytes(file.size) + ')'),
     h('input', {
-      'type': 'button',
-      'value': 'Remove',
-      'title': 'remove',
+      type: 'button',
+      value: 'Edit',
+      name: 'edit',
+      'ev-click': hg.send(file.channels.startEdit)
+    }),
+    h('input', {
+      type: 'button',
+      value: 'Remove',
+      name: 'remove',
       'ev-click': hg.send(parentHandles.remove, {
-        id: state.id
+        id: file.id
       })
     })
   ])
 }
 
 module.exports = File
+
+function FocusHook () {}
+
+FocusHook.prototype.hook = function hook (node, property) {
+  nextTick(function onTick () {
+    if (document.activeElement !== node) {
+      node.focus()
+    }
+  })
+}
